6f6f14fad3ea56bd7cc93cdde2f084c1748268ef
Tinotsu <tinopiof@gmail.com>
2025-12-04 20:46:18 +0100

    encoding thumbail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"encoding/base64"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -33,5 +35,56 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse maxMemory", err)
+		return
+	}
+
+	data, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't form file", err)
+		return
+	}
+
+	mediaType := fileHeader.Header.Get("Content-Type")
+
+	dataBytes, err := io.ReadAll(data)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read data", err)
+		return
+	}
+
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't read data", err)
+		return
+	}
+
+//	thumbnailImage := *new(thumbnail)
+//	thumbnailImage.data = dataBytes
+//	thumbnailImage.mediaType = mediaType
+//
+//	videoThumbnails[videoID] = thumbnailImage
+//
+//	port := os.Getenv("PORT")
+//	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", port, fmt.Sprintln(videoID))
+//	metadata.ThumbnailURL = &url
+//	err = cfg.db.UpdateVideo(metadata)
+//	if err != nil {
+//		respondWithError(w, http.StatusInternalServerError, "Couldn't read data", err)
+//		return
+//	}
+
+	// Encoding
+
+	dataString := base64.StdEncoding.EncodeToString(dataBytes)
+	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, dataString)
+
+	metadata.ThumbnailURL = &dataURL
+	err = cfg.db.UpdateVideo(metadata)
+
+	respondWithJSON(w, http.StatusOK, metadata)
 }
